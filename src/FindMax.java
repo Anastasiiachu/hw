8047190bfcb7eac16a94b96a9cb4c8b49bd6ae73
Anastasiiachu <chupanastasia@gmail.com>
2017-09-26 21:55:55 +0300

    Initial commit

---
+++ src/FindMax.java
@@ -0,0 +1,28 @@
+public class FindMax {
+    public int[] Price;
+    public FindMax(int[] Price) {
+        this.Price = Price;
+    }
+    public int sum(int left, int right) {
+        if (left > right) throw new IndexOutOfBoundsException("Not possible");
+        if (left == right) return Price[left];
+        int middle = (left + right) / 2;
+        int sumMaxLeft = 0;
+        int sumMaxRight = 0;
+        int currentSum = 0;
+        for (int i = middle; i >= left; i--) {
+            currentSum += Price[i];
+            sumMaxLeft = Math.max(currentSum, sumMaxLeft);
+        }
+        currentSum = 0;
+        for (int i = middle + 1; i <= right; i++) {
+            currentSum += Price[i];
+            sumMaxRight = Math.max(currentSum, sumMaxRight);
+        }
+        return Math.max(sumMaxLeft + sumMaxRight, Math.max(sum(left, middle), sum(middle + 1, right)));
+     }
+     public static void main(String[] args) {
+         FindMax arr = new FindMax(new int[] {112, 0, -36, 29, 8, -11, 7});
+         System.out.println(arr.sum(0, arr.Price.length - 1));
+     }
+}
